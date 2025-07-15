d7ad739d1ce8df955ec64fdf2ffd5233ca397003
ar1ess <webbis@internet.ru>
2025-07-15 13:37:44 +0300

    fix hover cta

--- assets/index-DPaLzieJ.js
+++ assets/index-DPaLzieJ.js
@@ -12666,4 +12666,4 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log("DOM полностью загружен");
   initMobileCoursesNavigation();
 });
-//# sourceMappingURL=index-8fgXqdln.js.map
+//# sourceMappingURL=index-DPaLzieJ.js.map
