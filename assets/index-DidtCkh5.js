0c27789d7e399c2c9068db4c540af39055082109
ar1ess <webbis@internet.ru>
2025-07-15 12:42:55 +0300

    center hero

--- assets/index-DidtCkh5.js
+++ assets/index-DidtCkh5.js
@@ -12666,4 +12666,4 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log("DOM полностью загружен");
   initMobileCoursesNavigation();
 });
-//# sourceMappingURL=index-DLEnpfpq.js.map
+//# sourceMappingURL=index-DidtCkh5.js.map
