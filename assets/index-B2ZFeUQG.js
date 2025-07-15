ca330dc7721c399cafff637afc5f5fda0af97c2d
ar1ess <webbis@internet.ru>
2025-07-15 13:26:52 +0300

    fix hero padding

--- assets/index-B2ZFeUQG.js
+++ assets/index-B2ZFeUQG.js
@@ -12666,4 +12666,4 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log("DOM полностью загружен");
   initMobileCoursesNavigation();
 });
-//# sourceMappingURL=index-y8XZ4igo.js.map
+//# sourceMappingURL=index-B2ZFeUQG.js.map
